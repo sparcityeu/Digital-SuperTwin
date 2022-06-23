78de3b43ae1a4cc560b2e35423b1435428e8690b
fatihtasyaran <fatihtasyaran@sabanciuniv.edu>
2022-06-23 03:42:28 +0000

    Per command observation recording added

--- twinned_run/instantiate.py
+++ twinned_run/instantiate.py
@@ -42,7 +42,8 @@ def generate_pcp2influxdb_config(config_file, tag):
 
     config_lines = ["[options]" + "\n",
                     "influx_server = http://127.0.0.1:8086" + "\n",
-                    "influx_db = " "pcp_" + tag + "\n",
+                    "influx_db = " + "pcp_" + tag + "\n",
+                    #"influx_tags = " + tag + "\n" 
                     "\n\n",
                     "[configured]" + "\n"]
 
@@ -51,7 +52,7 @@ def generate_pcp2influxdb_config(config_file, tag):
         config_lines.append(metric + " = ,," + "\n")
 
         
-    pcp_conf_name = "pcp" + tag + ".conf" 
+    pcp_conf_name = "pcp_" + tag + ".conf" 
     writer = open(pcp_conf_name, "w")
     
     for line in config_lines:
@@ -66,11 +67,6 @@ def run(config_file):
 
     command = "sleep 30"
     
-    start_timestamp = 1
-    end_timestamp = 1
-
-    
-
     
 def get_mongo_database(mongodb_name):
 
@@ -81,13 +77,77 @@ def get_mongo_database(mongodb_name):
     ##Create the database for this instance(s)
     return client[mongodb_name]
 
+def get_influx_database(influxdb_name):
+
+    influxdb = InfluxDBClient(host="localhost", port=8086)
+    influxdb.create_database(influxdb_name)
 
 
+def run_single(hostname, date, config_file, dt_pruned, command):
+
+    conf_prefix = config_file.replace(".", "_")
+    same_tag = get_date_tag()
+        
+    ##Get mongodb
+    mongodb_name = conf_prefix + same_tag
+    mongodb = get_mongo_database(mongodb_name)
+    collection = mongodb[mongodb_name + "_0"]
+    ##Get mongodb
+        
+    ##Get influxdb
+    pcp_conf_name, influxdb_name = generate_pcp2influxdb_config(config_file, same_tag)
+    get_influx_database(influxdb_name)
+    ##Get influxdb
+
+    
+    ##This is where actual thing happens
+    #####################################################################
+    p0_command = "pcp2influxdb -t 1 -c " + pcp_conf_name + " :configured"
+    #print("p0_command:", p0_command)
+    p0_args = shlex.split(p0_command)
+    #print("p0_args:", p0_args)
+
+    p1_command = command
+    p1_args = shlex.split(p1_command)
+
+    
+    p0 = Popen(p0_args)
+    p1 = Popen(p1_args, stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+    p1.wait()
+    p0.kill()
+    #####################################################################
+
+    
+    ##Connect target process metrics
+    pname = "_" + str(p1.pid) + "_" + p1_args[0] ##Name of the executed command
+    p_dtid = create_dt.get_uid(hostname, "process", "", 1)
+    for item in dt_pruned[p_dtid]["contents"]:
+        item["displayName"] = pname
+    ##Connect target process metrics
+    
+    metadata = {
+        "hostname": hostname,
+        "date": date,
+        #"dt_base": dt_base,
+        "dt_pruned": dt_pruned,
+        "influxdb": influxdb_name,
+        "command": command,
+        "command_options": p1_args
+    }
+    collection.insert_one(metadata)
+
+
+def run_commands(hostname, date, config_file, dt_pruned, commands):
+    
+    for command in commands:
+        
+        run_single(hostname, date, config_file, dt_pruned, command)
+    
+    
 def main():
 
     config_file = sys.argv[1]
-    print("here, config_file:", config_file)
-    
+
     #dt_base = create_dt.main("")
     dt_pruned = create_dt.main(config_file)
 
@@ -97,59 +157,22 @@ def main():
     date = datetime.datetime.now()
     date = date.strftime("%d-%m-%Y")
 
-    conf_prefix = config_file.replace(".", "_")
-    mongodb_name = conf_prefix + get_date_tag()
+    
 
-    mongodb = get_mongo_database(mongodb_name)
-    collection = mongodb[mongodb_name + "_0"]
-
-
-    pcp_conf_name, influx_db_name = generate_pcp2influxdb_config(config_file, get_date_tag())
-
-    influxdb = InfluxDBClient(host="localhost", port=8086)
-    influxdb.create_database(influx_db_name)
-
-
-    p0_command = "pcp2influxdb -t 1 -c " + pcp_conf_name + " :configured"
-    print("p0_command:", p0_command)
-    p0_args = shlex.split(p0_command)
-    print("p0_args:", p0_args)
+    commands = ["sleep 1",
+                "sleep 2",
+                "sleep 3",
+                "sleep 4",
+                "sleep 5",
+                "sleep 6",
+                "sleep 7",
+                "sleep 8",
+                "sleep 9",
+                "sleep 10"]
     
     
-    p1_command = "sleep 10"
-    p1_args = shlex.split(p1_command)
+    run_commands(hostname, date, config_file, dt_pruned, commands)
     
-    
-    
-    p0 = Popen(p0_args)
-
-    print("Will sleep")
-    p1 = Popen(p1_args, stdin = subprocess.PIPE, stdout = subprocess.PIPE)
-    p1.wait()
-    p0.kill()
-    
-    '''
-    print("Should kill: ", p0.pid)
-    p2 = Popen(["kill", str(p0.pid)], shell=False,
-              stdin = None, stdout = None, stderr = None, close_fds = True)
-    print("Killed! : ", p0.pid)
-    '''
-
-    ##Connect target process metrics
-    pname = "_" + str(p1.pid) + "_sleep"
-    p_dtid = create_dt.get_uid(hostname, "process", "", 1)
-    for item in dt_pruned[p_dtid]["contents"]:
-        item["displayName"] = pname
-    ##Connect target process metrics
-    
-    try0 = {
-        "hostname": hostname,
-        "date": date,
-        #"dt_base": dt_base,
-        "dt_pruned": dt_pruned,
-        "influxdb": influx_db_name
-    }
-    collection.insert_one(try0)
     
 if __name__ == "__main__":
 
