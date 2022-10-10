1cf0f0096e436c9f2ed7043d02fff1fc76a7c49b
fatihtasyaran <fatihtasyaran@sabanciuniv.edu>
2022-10-10 14:38:22 +0300

    Big structural change 2

--- structured/SuperTwin/probing/benchmarks/STREAM/clean.sh
+++
@@ -1,2 +0,0 @@
-rm stream.omp.AVX512.80M.20x.icc
-rm t*
--- structured/SuperTwin/probing/benchmarks/hpcg/tools/symgs.m
+++
@@ -1,8 +0,0 @@
-function [x] = symgs(A,y)
-LA = tril(A);
-UA = triu(A);
-DA = diag(diag(A));
-x = LA\y;
-x1 = y - LA*x + DA*x;
-x = UA\x1;
-end
