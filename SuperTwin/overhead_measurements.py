784a3ffb6929b79dce56ed61cc64c136e691aa70
fatihtasyaran <fatihtasyaran@sabanciuniv.edu>
2022-12-16 04:14:18 +0300

    Measurement is all well

--- SuperTwin/overhead_measurements.py
+++ SuperTwin/overhead_measurements.py
@@ -96,6 +96,8 @@ def one_run_two_returns(client, interval, metric1, metric2, fields, sampler_conf
     mem_overheads = {}
     cpu_responses = {}
     mem_responses = {}
+    io_overheads = {}
+    io_responses = {}
     net_traffic = {}
     
     for key in fields:
@@ -103,6 +105,8 @@ def one_run_two_returns(client, interval, metric1, metric2, fields, sampler_conf
         mem_overheads[key] = []
         cpu_responses[key] = []
         mem_responses[key] = []
+        io_overheads[key] = []
+        io_responses[key] = []
     net_traffic["all"] = []
             
     for i in range(5):
@@ -134,18 +138,22 @@ def one_run_two_returns(client, interval, metric1, metric2, fields, sampler_conf
         for key in fields:
             cpu_query = 'SELECT ' + '"' + fields[key] + '"' +' from ' + metric1
             mem_query = 'SELECT ' + '"' + fields[key] + '"' +' from ' + metric2
+            io_query = 'SELECT ' + '"' + fields[key] + '"' +' from ' + "proc_io_total_bytes"
             print("cpu_query:", cpu_query)
-            #print("mem_query:", mem_query)
+            print("mem_query:", mem_query)
+            print("io_query:", io_query)
             #print("key:", key)
             #print("q:", client.query(cpu_query))
             #print("w:", client.query(mem_query))
             try:
                 cpu_responses[key] = list(client.query(cpu_query))[0]
                 mem_responses[key] = list(client.query(mem_query))[0]
+                io_responses[key] = list(client.query(io_query))[0]
             except:
                 sample(interval, sampler_config, duration)
                 cpu_responses[key] = list(client.query(cpu_query))[0]
                 mem_responses[key] = list(client.query(mem_query))[0]
+                io_responses[key] = list(client.query(io_query))[0]
 
                 
                 
@@ -176,11 +184,20 @@ def one_run_two_returns(client, interval, metric1, metric2, fields, sampler_conf
                 _sum2 += item[fields[key]]
             _sum2 /= len(mem_responses[key])
             mem_overheads[key].append(_sum2)
+
+            _sum3 = 0
+            for item in mem_responses[key]:
+                #print("item:", item)
+                #print("fields[key]:", fields[key])
+                _sum3 += item[fields[key]]
+            _sum3 /= len(io_responses[key])
+            _sum3 /= 1024 ##convert to kbs as others
+            io_overheads[key].append(_sum3)
         
             #print("Mean CPU usage of",field, _sum1)
             #print("Mean MEMORY usage of",field, _sum2) 
             
-    return cpu_overheads, mem_overheads, net_traffic
+    return cpu_overheads, mem_overheads, io_overheads, net_traffic
     
 def bw():
 
@@ -217,6 +234,40 @@ def main(addr, config, name, run_name, alias):
     
     client = InfluxDBClient(host='localhost', port=8086)
 
+    
+    sampler_config = config
+    metric1 = "cpu_use"
+    metric2 = "mem_use"
+    fields = pmdas
+
+    duration = 20
+    
+    _runs = {}
+    _gots = {}
+    _ios = {}
+    _nets = {}
+
+    
+    my_superTwin.reconfigure_observation_events_parameterized("dolap10_perfevent.txt")
+    fields = reassign_pids(my_superTwin)
+    _runs["1"], _gots["1"], _ios["1"], _nets["1"] = one_run_two_returns(client, "1", metric1, metric2, fields, sampler_config, duration, name)
+    #my_superTwin.reconfigure_observation_events_parameterized("dolap10_perfevent.txt")
+    fields = reassign_pids(my_superTwin)
+    _runs["0.5"], _gots["0.5"], _ios["0.5"], _nets["0.5"] = one_run_two_returns(client, "0.5", metric1, metric2, fields, sampler_config, duration, name)
+    #my_superTwin.reconfigure_observation_events_parameterized("dolap10_perfevent.txt")
+    fields = reassign_pids(my_superTwin)
+    _runs["0.25"], _gots["0.25"], _ios["0.25"], _nets["0.25"] = one_run_two_returns(client, "0.25", metric1, metric2, fields, sampler_config, duration, name)
+    #my_superTwin.reconfigure_observation_events_parameterized("dolap10_perfevent.txt")
+    fields = reassign_pids(my_superTwin)
+    _runs["0.125"], _gots["0.125"], _ios["0.125"], _nets["0.125"] = one_run_two_returns(client, "0.125" , metric1, metric2, fields, sampler_config, duration, name)
+    #my_superTwin.reconfigure_observation_events_parameterized("dolap10_perfevent.txt")
+    fields = reassign_pids(my_superTwin)
+    _runs["0.0625"], _gots["0.0625"], _ios["0.0625"], _nets["0.0625"] = one_run_two_returns(client, "0.0625" , metric1, metric2, fields, sampler_config, duration, name)
+    #my_superTwin.reconfigure_observation_events_parameterized("dolap10_perfevent.txt")
+    fields = reassign_pids(my_superTwin)
+    _runs["0.03125"], _gots["0.03125"], _ios["0.03125"], _nets["0.03125"] = one_run_two_returns(client, "0.03125" , metric1, metric2, fields, sampler_config, duration, name)
+
+
     ##datapoints
     client.switch_database("dolap_run")
     mes = list(client.query("SHOW MEASUREMENTS"))[0]
@@ -227,46 +278,16 @@ def main(addr, config, name, run_name, alias):
         print(item["name"], ":", datapoints)
         total_datapoints += datapoints
     ##
+    total_datapoints = str(total_datapoints)
     print("Total datapoints:", total_datapoints)
-    exit(1)
-        
-    sampler_config = config
-    metric1 = "cpu_use"
-    metric2 = "mem_use"
-    fields = pmdas
-
-    duration = 20
     
-    _runs = {}
-    _gots = {}
-    _nets = {}
-
-    
-    my_superTwin.reconfigure_observation_events_parameterized("dolap10_perfevent.txt")
-    fields = reassign_pids(my_superTwin)
-    _runs["1"], _gots["1"], _nets["1"] = one_run_two_returns(client, "1", metric1, metric2, fields, sampler_config, duration, name)
-    #my_superTwin.reconfigure_observation_events_parameterized("dolap10_perfevent.txt")
-    fields = reassign_pids(my_superTwin)
-    _runs["0.5"], _gots["0.5"], _nets["0.5"] = one_run_two_returns(client, "0.5", metric1, metric2, fields, sampler_config, duration, name)
-    #my_superTwin.reconfigure_observation_events_parameterized("dolap10_perfevent.txt")
-    fields = reassign_pids(my_superTwin)
-    _runs["0.25"], _gots["0.25"], _nets["0.25"] = one_run_two_returns(client, "0.25", metric1, metric2, fields, sampler_config, duration, name)
-    #my_superTwin.reconfigure_observation_events_parameterized("dolap10_perfevent.txt")
-    fields = reassign_pids(my_superTwin)
-    _runs["0.125"], _gots["0.125"], _nets["0.125"] = one_run_two_returns(client, "0.125" , metric1, metric2, fields, sampler_config, duration, name)
-    #my_superTwin.reconfigure_observation_events_parameterized("dolap10_perfevent.txt")
-    fields = reassign_pids(my_superTwin)
-    _runs["0.0625"], _gots["0.0625"], _nets["0.0625"] = one_run_two_returns(client, "0.0625" , metric1, metric2, fields, sampler_config, duration, name)
-    #my_superTwin.reconfigure_observation_events_parameterized("dolap10_perfevent.txt")
-    fields = reassign_pids(my_superTwin)
-    _runs["0.03125"], _gots["0.03125"], _nets["0.03125"] = one_run_two_returns(client, "0.03125" , metric1, metric2, fields, sampler_config, duration, name)
-            
 
     writer = open("dolap_results" + ".csv", "a")
     for key in _runs:
         cpuo = _runs[key]
         memo = _gots[key]
         neto = _nets[key]
+        io = _ios[key]
         interval = key
         #print("cpuo:", cpuo, "memo:", memo, "key:", key)
         for comp in cpuo:
@@ -281,8 +302,13 @@ def main(addr, config, name, run_name, alias):
             mem_use_min = str(min(memo[comp]))
             mem_use_max = str(max(memo[comp]))
             mem_use_std = str(statistics.stdev(cpuo[comp]))
+
+            io_use_mean = str(statistics.mean(io[comp]))
+            io_use_min = str(min(io[comp]))
+            io_use_max = str(max(io[comp]))
+            io_use_std = str(statistics.stdev(io[comp]))
             
-            line = alias + "," + interval + "," + comp + "," + cpu_use_mean + "," + cpu_use_min + "," + cpu_use_max + "," + cpu_use_std + "," + mem_use_mean + "," + mem_use_min + "," + mem_use_max + "," + mem_use_std + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "\n"
+            line = alias + "," + interval + "," + comp + "," + cpu_use_mean + "," + cpu_use_min + "," + cpu_use_max + "," + cpu_use_std + "," + mem_use_mean + "," + mem_use_min + "," + mem_use_max + "," + mem_use_std + "," + io_use_mean + "," + io_use_min + "," + io_use_max + "," + io_use_std + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1"+"\n" 
             writer.write(line)
 
         net_use_mean = str(statistics.mean(neto["all"]))
@@ -290,7 +316,10 @@ def main(addr, config, name, run_name, alias):
         net_use_max = str(max(neto["all"]))
         net_use_std = str(statistics.stdev(neto["all"]))
 
-        line = alias + "," + interval + "," + "network" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + net_use_mean + "," + net_use_min + "," + net_use_max + "," + net_use_std + "\n"
+        line = alias + "," + interval + "," + "network" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + net_use_mean + "," + net_use_min + "," + net_use_max + "," + net_use_std + "," + "-1" +"\n"
+        writer.write(line)
+
+        line = alias + "," + interval + "," + "datapoints" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + "-1" + "," + total_datapoints +"\n"
         writer.write(line)
     
     writer.close()
@@ -311,14 +340,14 @@ def main(addr, config, name, run_name, alias):
         
 if __name__ == "__main__":
 
-    #print("With 10 metrics")
-    #main("10.36.54.195", " -c overhead_configs/dolap_10.conf :configured", "dolap", "try0", "dolap10")
-    #print("With 20 metrics")
-    #main("10.36.54.195", " -c overhead_configs/dolap_20.conf :configured", "dolap", "try0", "dolap20")
-    #print("With 30 metrics")
-    #main("10.36.54.195", " -c overhead_configs/dolap_30.conf :configured", "dolap", "try0", "dolap30")
-    #print("With 40 metrics")
-    #main("10.36.54.195", " -c overhead_configs/dolap_40.conf :configured", "dolap", "try0", "dolap40")
+    print("With 10 metrics")
+    main("10.36.54.195", " -c overhead_configs/dolap_10.conf :configured", "dolap", "try0", "dolap10")
+    print("With 20 metrics")
+    main("10.36.54.195", " -c overhead_configs/dolap_20.conf :configured", "dolap", "try0", "dolap20")
+    print("With 30 metrics")
+    main("10.36.54.195", " -c overhead_configs/dolap_30.conf :configured", "dolap", "try0", "dolap30")
+    print("With 40 metrics")
+    main("10.36.54.195", " -c overhead_configs/dolap_40.conf :configured", "dolap", "try0", "dolap40")
     print("With 50 metrics")
     main("10.36.54.195", " -c overhead_configs/dolap_50.conf :configured", "dolap", "try0", "dolap50")
     print("#########################################################################################")
