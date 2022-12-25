56acd9b6c17300acaf87828dc99c0b8004d9b7c3
fatihtasyaran <fatihtasyaran@sabanciuniv.edu>
2022-12-25 17:46:11 +0300

    accuracy table modular

--- SuperTwin/accuracy_overhead_throughput/accuracy_table.py
+++ SuperTwin/accuracy_overhead_throughput/accuracy_table.py
@@ -11,18 +11,35 @@ import statistics
 def ret_list(df, no_metric, freq):
 
     df = df.loc[(df["no_metric"] == no_metric) & (df["frequency"] == freq)]
+    print("no_metric:", no_metric, "freq:", freq)
+    print("df there:", df)
     df = df["error"]
+    print("last df:", df)
     return list(df)
     
 
 def ret_metric(df, metric, no_metric, freq):
 
     df = df.loc[df["metric"] == metric]
+    print("df here:", df)
     df = ret_list(df, no_metric, freq)
-
+    
     df = [abs(x) for x in df]
     
+    
     return statistics.mean(df)
+
+def ret_trace(_data, metric, no_metrics, freq, name):
+    line = []
+    for freq in freqs:
+        line.append(ret_metric(_dolap_data, "fp_arith_scalar_double_err", 8, freq))
+        
+        trace1 = (go.Scatter(x=xx, y=line,
+                             mode='lines',
+                             name="Dolap-8",
+                             line=dict(color=dolap_colors_4[0], width=4)))
+
+    return trace1
 
 deren_colors_4 = ["rgb(182,156,253)", "rgb(149,38,92)", "rgb(183,200,226)", "rgb(66,65,117)", "rgb(247,94,240)"] 
 dolap_colors_4 = ["rgb(6,150,104)", "rgb(195,222,155)", "rgb(52,75,70)", "rgb(121,235,153)", "rgb(101,139,131)"]
@@ -63,83 +80,172 @@ print(ret_metric(_dolap_data, "l1_bw", 24, 8))
 
 #metrics = [1,4,8,12,16,20,24]
 metrics = [8,16,24]
-freqs = [1,2,4,8,16,32]
+freqs = [1,2,4,8,16]
 
 xx = ["1", "1/2", "1/4", "1/8", "1/16", "1/32"]
 
 fig = go.Figure()
 fig = make_subplots(rows=2, cols=2, column_widths=[0.5,0.5], row_heights=[0.5, 0.5], horizontal_spacing=0.1, shared_xaxes=True)
-for idx, metric in enumerate(metrics):
-    line = []
-    for freq in freqs:
-        line.append(ret_metric(_dolap_data, "fp_arith_scalar_double_err", metric, freq))
+
+line = []
+for freq in freqs:
+    line.append(ret_metric(_dolap_data, "fp_arith_scalar_double_err", 8, freq))
         
     trace1 = (go.Scatter(x=xx, y=line,
-                             mode='lines',
-                             name=str(metric) + " metric",
-                             line=dict(color=seven_colors[6-idx], width=2)))
+                         mode='lines',
+                         name="Dolap-8",
+                         line=dict(color=dolap_colors_4[0], width=4)))
     
 
-    fig.append_trace(trace1, row=1, col=1)
+fig.append_trace(trace1, row=1, col=1)
 
-'''
-fig = go.Figure()
-metrics = [4,8,12,16,20,24]
-
-for metric in metrics:
-    line = []
-    for freq in freqs:
-        line.append(ret_metric(_dolap_data, "l1_bw", metric, freq))
-        trace2 = (go.Scatter(x=xx, y=line,
-                             mode='lines',
-                             name=str(metric) + " metric",
-                             line=dict(color=seven_colors, width=2)))
+line = []
+for freq in freqs:
+    line.append(ret_metric(_dolap_data, "fp_arith_scalar_double_err", 16, freq))
+        
+    trace1 = (go.Scatter(x=xx, y=line,
+                         mode='lines',
+                         name="Dolap-16",
+                         line=dict(color=dolap_colors_4[2], width=4, dash="dash")))
     
+
+fig.append_trace(trace1, row=1, col=1)
     
-fig.show()
-
-fig = go.Figure()
-metrics = [4,8,12,16,20,24]
-
-for metric in metrics:
-    line = []
-    for freq in freqs:
-        line.append(ret_metric(_dolap_data, "instruction_retired_err", metric, freq))
-    fig.add_trace(go.Scatter(x=xx, y=line,
-                             mode='lines',
-                             name=str(metric) + " metric",
-                             line=dict(color=luna_colors_4, width=2)))
+line = []
+for freq in freqs:
+    line.append(ret_metric(_dolap_data, "fp_arith_scalar_double_err", 24, freq))
+        
+    trace1 = (go.Scatter(x=xx, y=line,
+                         mode='lines',
+                         name="Dolap-24",
+                         line=dict(color=dolap_colors_4[4], width=4, dash="dot")))
     
+
+fig.append_trace(trace1, row=1, col=1)
+
+##
+##
+poseidon_24_1 = _poseidon_data.loc[(_poseidon_data["no_metric"] == 8) & (_poseidon_data["frequency"] == 32) & (_poseidon_data["metric"] == "fp_arith_scalar_double_err")]
+print("asked:", poseidon_24_1)
+line = []
+for freq in freqs:
+    line.append(ret_metric(_poseidon_data, "fp_arith_scalar_double_err", 8, freq))
+        
+    trace1 = (go.Scatter(x=xx, y=line,
+                         mode='lines',
+                         name="Poseidon-8",
+                         line=dict(color=poseidon_colors_4[0], width=4)))
     
-fig.show()
 
-fig = go.Figure()
-metrics = [8,12,16,20,24]
+fig.append_trace(trace1, row=1, col=1)
 
-for metric in metrics:
-    line = []
-    for freq in freqs:
-        line.append(ret_metric(_dolap_data, "unhalted_reference_cycles_err", metric, freq))
-    fig.add_trace(go.Scatter(x=xx, y=line,
-                             mode='lines',
-                             name=str(metric) + " metric",
-                             line=dict(color=poseidon_colors_4, width=2)))
+line = []
+for freq in freqs:
+    line.append(ret_metric(_poseidon_data, "fp_arith_scalar_double_err", 16, freq))
+        
+    trace1 = (go.Scatter(x=xx, y=line,
+                         mode='lines',
+                         name="Poseidon-16",
+                         line=dict(color=poseidon_colors_4[2], width=4, dash="dash")))
     
-fig.show()
 
-
-fig = go.Figure()
-for metric in metrics:
-    line = []
-    for freq in freqs:
-        line.append(ret_metric(_dolap_data, "uops_retired_any_err", metric, freq))
-    fig.add_trace(go.Scatter(x=xx, y=line,
-                             mode='lines',
-                             name=str(metric) + " metric"))
+fig.append_trace(trace1, row=1, col=1)
     
-fig.show()
-'''
+line = []
+for freq in freqs:
+    line.append(ret_metric(_poseidon_data, "fp_arith_scalar_double_err", 24, freq))
+        
+    trace1 = (go.Scatter(x=xx, y=line,
+                         mode='lines',
+                         name="Poseidon-24",
+                         line=dict(color=poseidon_colors_4[4], width=4, dash="dot")))
+    
 
+fig.append_trace(trace1, row=1, col=1)
+
+##
+##
+
+line = []
+for freq in freqs:
+    line.append(ret_metric(_deren_data, "fp_arith_scalar_double_err", 8, freq))
+        
+    trace1 = (go.Scatter(x=xx, y=line,
+                         mode='lines',
+                         name="Deren-8",
+                         line=dict(color=deren_colors_4[0], width=4)))
+    
+
+fig.append_trace(trace1, row=1, col=1)
+
+line = []
+for freq in freqs:
+    line.append(ret_metric(_deren_data, "fp_arith_scalar_double_err", 16, freq))
+        
+    trace1 = (go.Scatter(x=xx, y=line,
+                         mode='lines',
+                         name="Deren-16",
+                         line=dict(color=poseidon_colors_4[2], width=4, dash="dash")))
+    
+
+fig.append_trace(trace1, row=1, col=1)
+    
+line = []
+for freq in freqs:
+    line.append(ret_metric(_deren_data, "fp_arith_scalar_double_err", 24, freq))
+        
+    trace1 = (go.Scatter(x=xx, y=line,
+                         mode='lines',
+                         name="Deren-24",
+                         line=dict(color=poseidon_colors_4[4], width=4, dash="dot")))
+    
+
+fig.append_trace(trace1, row=1, col=1)
+
+##
+##
+
+line = []
+for freq in freqs:
+    line.append(ret_metric(_luna_data, "fp_arith_scalar_double_err", 8, freq))
+        
+    trace1 = (go.Scatter(x=xx, y=line,
+                         mode='lines',
+                         name="Luna-8",
+                         line=dict(color=luna_colors_4[0], width=4)))
+    
+
+fig.append_trace(trace1, row=1, col=1)
+
+line = []
+for freq in freqs:
+    line.append(ret_metric(_luna_data, "fp_arith_scalar_double_err", 16, freq))
+        
+    trace1 = (go.Scatter(x=xx, y=line,
+                         mode='lines',
+                         name="Luna-16",
+                         line=dict(color=poseidon_colors_4[2], width=4, dash="dash")))
+    
+
+fig.append_trace(trace1, row=1, col=1)
+    
+line = []
+for freq in freqs:
+    line.append(ret_metric(_deren_data, "fp_arith_scalar_double_err", 24, freq))
+        
+    trace1 = (go.Scatter(x=xx, y=line,
+                         mode='lines',
+                         name="Luna-24",
+                         line=dict(color=poseidon_colors_4[4], width=4, dash="dot")))
+    
+
+fig.append_trace(trace1, row=1, col=1)
+
+
+
+############################################################################################
+############################################################################################
+############################################################################################
 fig.update_layout(template="simple_white")
 fig.update_layout(legend=dict(yanchor="top",y=1.075,xanchor="left",x=0.01,orientation="h"))
 
